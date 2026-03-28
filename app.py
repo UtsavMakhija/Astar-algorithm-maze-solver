17843de0c48965a254ea958e45b5d2d16b1416cd
UtsavMakhija <utsav.25bce10713@vitbhopal.ac.in>
2026-03-28 10:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import numpy as np
+import streamlit as st
+import time
+import heapq
+import pandas as pd
 
+st.set_page_config(layout = 'centered')
+st.title("AI MAZE SOLVER USING A* ALGORITHM")
+
+grid_size = 10
+
+# creating a random maze
+def create_grid():
+    return np.random.choice([0 , 1] , size = (grid_size , grid_size) , p = [0.7 , 0.3])
+
+# defining the heuristic function
+def heuristic(a , b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
